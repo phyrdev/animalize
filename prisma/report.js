265b08171fbedcacb22435085c953efba1402759
Priyangsu Banerjee <priyangsu26@gmail.com>
2024-06-16 12:44:17 +0530

    refactor: Order organization reports by createdAt in descending order

--- prisma/report.js
+++ prisma/report.js
@@ -69,6 +69,9 @@ export const getOrgReports = async (orgno) => {
       where: {
         orgno,
       },
+      orderBy: {
+        createdAt: "desc",
+      },
       include: {
         payment: true,
         organization: true,
