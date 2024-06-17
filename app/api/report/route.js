931b2dd63f3b9ff6900bd0856455ce98dd6a70c9
Priyangsu Banerjee <priyangsu26@gmail.com>
2024-06-17 13:13:46 +0530

    chore: Update npm dependencies and add Chromium and Chrome AWS Lambda packages

--- app/api/report/route.js
+++ app/api/report/route.js
@@ -1,4 +1,5 @@
-import puppeteer from "puppeteer";
+const chromium = require("@sparticuz/chromium");
+const puppeteer = require("puppeteer-core");
 
 export async function GET(request) {
   const { searchParams } = new URL(request.url);
@@ -7,7 +8,13 @@ export async function GET(request) {
   let url = `https://animalize.io/final-report/${id}/s`;
 
   async function generatePDF(url) {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+      args: chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath(),
+      headless: chromium.headless,
+      ignoreHTTPSErrors: true,
+    });
     const page = await browser.newPage();
     await page.goto(url, { waitUntil: "networkidle0" });
     await page.emulateMediaType("print");
