230d5271bf81c0eba78382cc7e57a127d94a9560
Priyangsu Banerjee <priyangsu26@gmail.com>
2024-06-17 18:04:35 +0530

    feat: Add html2canvas and jsPDF for generating PDF reports

--- app/final-report/[id]/download/page.js
+++ app/final-report/[id]/download/page.js
@@ -73,17 +73,9 @@ function FinalReport({ params }) {
       ) : (
         <>
           {report ? (
-            <div className="w-full bg-neutral-100 min-h-svh">
-              <div className="h-fit w-fit fixed top-0 -translate-y-full">
-                <div className="mt-5">
-                  <Button className="rounded-md" onClick={handlePrint}>
-                    Print invoice
-                  </Button>
-                </div>
-                <div
-                  ref={componentRef}
-                  className="w-[896px] bg-white h-fit px-10"
-                >
+            <div className="w-full">
+              <div className="h-fit w-fit">
+                <div className="w-[896px] bg-white h-fit px-10">
                   <div className="mt-10 space-y-16">
                     {report.tests.map((test, i) => {
                       return (
@@ -312,62 +304,6 @@ function FinalReport({ params }) {
                       );
                     })}
                   </div>
-                </div>
-              </div>
-              <div className="w-full bg-white max-w-lg mx-auto pt-7 pb-5">
-                <img src="/explogo.svg" className="w-36 mx-auto" alt="" />
-                <div className="flex items-center justify-between border-y border-dashed py-2 px-5 mt-7 text-sm">
-                  <span>
-                    {new Date(report.createdAt).toDateString("en-US", {
-                      weekday: "long",
-                      year: "numeric",
-                      month: "long",
-                    })}
-                  </span>
-                  <span>Report</span>
-                  <span>{report.reportno}</span>
-                </div>
-                <div className="mt-10 px-5">
-                  <p className="leading-7 text-sm text-neutral-700">
-                    Your report is ready for download. Please click the button
-                    below to print / download the report. If you have any issues
-                    or concerns, please contact us at{" "}
-                    <a
-                      className="text-blue-500"
-                      href={`mailto:${report.organization.email}`}
-                    >
-                      {report.organization.email}
-                    </a>
-                  </p>
-
-                  <p className="leading-7 text-sm text-neutral-700 mt-5">
-                    You can also get a physical copy of the report by visiting
-                    our office.
-                  </p>
-                  <Button
-                    className="rounded-md w-fit mt-10"
-                    onClick={handlePrint}
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="20"
-                      height="20"
-                      viewBox="0 0 24 24"
-                    >
-                      <g
-                        fill="none"
-                        stroke="currentColor"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="1.5"
-                        color="currentColor"
-                      >
-                        <path d="M7.354 18c-2.123 0-3.185 0-3.94-.453a3.04 3.04 0 0 1-1.15-1.223c-.392-.77-.287-1.787-.075-3.822c.176-1.698.264-2.547.698-3.171c.285-.41.67-.745 1.121-.977C4.695 8 5.582 8 7.354 8h9.292c1.772 0 2.659 0 3.346.354c.451.232.836.567 1.121.977c.434.624.522 1.473.698 3.172c.212 2.034.317 3.052-.076 3.821a3.04 3.04 0 0 1-1.148 1.223C19.83 18 18.769 18 16.646 18M17 8V6c0-1.886 0-2.828-.586-3.414S14.886 2 13 2h-2c-1.886 0-2.828 0-3.414.586S7 4.114 7 6v2" />
-                        <path d="M13.989 16H10.01c-.685 0-1.028 0-1.32.109a1.87 1.87 0 0 0-.945.8c-.168.281-.251.642-.417 1.363c-.26 1.128-.39 1.691-.301 2.143c.117.602.484 1.112.995 1.382c.382.203.918.203 1.988.203h3.978c1.07 0 1.606 0 1.988-.203c.51-.27.878-.78.995-1.382c.089-.452-.041-1.015-.3-2.143c-.167-.72-.25-1.082-.418-1.362a1.87 1.87 0 0 0-.946-.801C15.017 16 14.674 16 13.988 16M18 12h.009" />
-                      </g>
-                    </svg>
-                    <span>Print report</span>
-                  </Button>
                 </div>
               </div>
             </div>
