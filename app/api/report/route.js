4a33a66e3c459a2bcc2ac6abd42da785417c972a
Priyangsu Banerjee <priyangsu26@gmail.com>
2024-06-17 13:01:41 +0530

    refactor: Generate and redirect to PDF report for given ID

--- app/api/report/route.js
+++ app/api/report/route.js
@@ -4,27 +4,23 @@ export async function GET(request) {
   const { searchParams } = new URL(request.url);
   const id = searchParams.get("id");
 
-  let url =
-    "https://www.bannerbear.com/blog/how-to-download-images-from-a-website-using-puppeteer/";
+  let url = `https://animalize.io/final-report/${id}/s`;
 
-  async function generatePDF(url, outputPath) {
+  async function generatePDF(url) {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto(url, { waitUntil: "networkidle0" });
-    await page.emulateMediaType("screen");
-    const pdf = await page.pdf({
-      path: "/public/result.pdf",
-      margin: { top: "100px", right: "50px", bottom: "100px", left: "50px" },
+    await page.emulateMediaType("print");
+    await page.pdf({
+      path: `public/reports/${id}.pdf`,
       printBackground: true,
       format: "A4",
     });
     await browser.close();
   }
   try {
-    await generatePDF(url, "outputPath.pdf");
-    return Response.json({
-      message: "PDF generated successfully",
-    });
+    await generatePDF(url);
+    return Response.redirect(`https://animalize.io/reports/${id}.pdf`, 302);
   } catch (error) {
     console.log(error);
     return Response.json({
