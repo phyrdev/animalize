52c78d1869b3b231407dd0e5a1f6d1ca2282f9bb
Priyangsu Banerjee <priyangsu26@gmail.com>
2024-06-17 18:24:45 +0530

    refactor: Update PDF generation to use dynamic file name

--- app/api/report/route.js
+++ app/api/report/route.js
@@ -3,6 +3,8 @@ const fs = require("fs");
 
 export async function GET(req, res) {
   // create the API client instance
+
+  let id = "D7CUEJMRXU";
   var client = new pdfcrowd.HtmlToPdfClient(
     "demo",
     "ce544b6ea52a5621fb9d55f8b542d14d"
@@ -11,12 +13,12 @@ export async function GET(req, res) {
   // run the conversion and write the result to a file
   client.convertUrlToFile(
     "https://animalize.io/final-report/D7CUEJMRXU/download",
-    "public/result.pdf",
+    `public/${id}.pdf`,
     function (err, fileName) {
       if (err) return console.error("Pdfcrowd Error: " + err);
       console.log("Success: the file was created " + fileName);
     }
   );
 
-  return Response.redirect(`http://localhost:3000/result.pdf`);
+  return Response.redirect(`https://animalize.io/${id}.pdf`);
 }
