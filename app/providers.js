c1e70619f86eae0b117737cdd4c1f18f4ae3c950
Priyangsu Banerjee <priyangsu26@gmail.com>
2024-06-22 16:07:03 +0530

    feat: Add next-nprogress-bar package for progress bar functionality

--- app/providers.js
+++ app/providers.js
@@ -5,11 +5,19 @@ import { SessionProvider, useSession } from "next-auth/react";
 import { NextUIProvider } from "@nextui-org/react";
 import { Toaster } from "react-hot-toast";
 import Content from "./content";
+import { AppProgressBar as ProgressBar } from "next-nprogress-bar";
 
 export function Providers({ children }) {
   return (
     <SessionProvider>
       <NextUIProvider>
+        <ProgressBar
+          color="#000"
+          options={{
+            showSpinner: true,
+          }}
+          shallowRouting={true}
+        />
         <Content>{children}</Content>
       </NextUIProvider>
       <Toaster
